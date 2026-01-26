d65af230f5e3bb8b620e0b8dcf80aa16e0cac24d
Darien-Modus <darien@modusdesign.no>
2026-01-26 10:34:43 +0100

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
+if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables! Check your .env file.');
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
